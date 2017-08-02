735e811abc8670e10592ef6cc66d474031ef88a0
Jesse Carpenter <30635253+Carpenter-Software@users.noreply.github.com>
2017-08-01 22:15:21 -0600

    Create main.h

---
+++ FIRST-TEST/main.h
@@ -0,0 +1,31 @@
+// TODO WORKING FILE
+
+#ifndef MAIN_H
+#define MAIN_H
+
+// bcm 28325 GPIO interface 
+#include <bcm2835.h>
+
+// Supports file I/O
+#include <stdio.h>
+
+// Miscellaneous Declaations
+#include <stdlib.h>
+
+// 
+// #include <math.h> 
+
+// 
+// #include <string.h> 
+
+// 
+// #include <time.h> 
+
+// 
+// #include <signal.h 
+
+// 
+// #include <stddef.h> 
+
+
+#endif
